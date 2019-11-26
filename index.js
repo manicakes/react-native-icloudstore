16524b25efa47b348b9832612f7715b0647db506
Tagir Almasov <tccup1@gmail.com>
2019-11-26 17:29:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import { AsyncStorage, NativeModules, Platform } from 'react-native';
 
-const iCloudStorage = Platform.OS === 'ios' ? NativeModules.RNICloudStorage : AsyncStorage;
+const iCloudStorage = Platform.OS === 'ios' || Platform.isTVOS ? NativeModules.RNICloudStorage : AsyncStorage;
 
 export default iCloudStorage;
 
